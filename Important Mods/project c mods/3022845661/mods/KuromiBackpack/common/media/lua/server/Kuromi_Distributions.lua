c89678c2fe680a89817aef3cb0072a7ebc9b38b4
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-08-23 04:33:47 +0900

    [Chore] Add mod 3022845661

---
+++ Important Mods/project c mods/3022845661/mods/KuromiBackpack/common/media/lua/server/Kuromi_Distributions.lua
@@ -0,0 +1,219 @@
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, "kuromibackpack.kuromi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, "kuromibackpack.kuromi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, "kuromibackpack.kuromi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, "kuromibackpack.kuromi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.kuromi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, "kuromibackpack.kuromi_backpack");
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.kuromi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, "kuromibackpack.kuromi_backpack");
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, "kuromibackpack.kuromi_backpack");
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 0.01);
+
+
+
+
+
+
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, "kuromibackpack.hellokitty_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, "kuromibackpack.hellokitty_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, "kuromibackpack.hellokitty_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, "kuromibackpack.hellokitty_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.hellokitty_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, "kuromibackpack.hellokitty_backpack");
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.hellokitty_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, "kuromibackpack.hellokitty_backpack");
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, "kuromibackpack.hellokitty_backpack");
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 0.01);
+
+
+
+
+
+
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, "kuromibackpack.mymelody_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, "kuromibackpack.mymelody_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, "kuromibackpack.mymelody_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, "kuromibackpack.mymelody_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.mymelody_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, "kuromibackpack.mymelody_backpack");
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.mymelody_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, "kuromibackpack.mymelody_backpack");
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, "kuromibackpack.mymelody_backpack");
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 0.01);
+
+
+
+
+
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, "kuromibackpack.cinnamoroll_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, "kuromibackpack.cinnamoroll_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, "kuromibackpack.cinnamoroll_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, "kuromibackpack.cinnamoroll_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.cinnamoroll_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, "kuromibackpack.cinnamoroll_backpack");
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.cinnamoroll_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, "kuromibackpack.cinnamoroll_backpack");
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, "kuromibackpack.cinnamoroll_backpack");
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 0.01);
+
+
+
+
+
+
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, "kuromibackpack.badtzmaru_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, "kuromibackpack.badtzmaru_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, "kuromibackpack.badtzmaru_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, "kuromibackpack.badtzmaru_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.badtzmaru_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, "kuromibackpack.badtzmaru_backpack");
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.badtzmaru_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, "kuromibackpack.badtzmaru_backpack");
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, "kuromibackpack.badtzmaru_backpack");
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 0.01);
+
+
+
+
+
+
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, "kuromibackpack.Keroppi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, "kuromibackpack.Keroppi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, "kuromibackpack.Keroppi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, "kuromibackpack.Keroppi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.Keroppi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, "kuromibackpack.Keroppi_backpack");
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.Keroppi_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, "kuromibackpack.Keroppi_backpack");
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, "kuromibackpack.Keroppi_backpack");
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 0.01);
+
+
+
+
+
+
+
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, "kuromibackpack.pompompurin_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresDress"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, "kuromibackpack.pompompurin_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageAllJackets"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, "kuromibackpack.pompompurin_backpack");
+table.insert(ProceduralDistributions.list["ClothingStorageLegwear"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, "kuromibackpack.pompompurin_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresSummer"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.pompompurin_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, "kuromibackpack.pompompurin_backpack");
+table.insert(ProceduralDistributions.list["WardrobeGeneric"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, "kuromibackpack.pompompurin_backpack");
+table.insert(ProceduralDistributions.list["ClothingStoresWoman"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, "kuromibackpack.pompompurin_backpack");
+table.insert(ProceduralDistributions.list["GigamartSchool"].items, 0.1);
+
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, "kuromibackpack.pompompurin_backpack");
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 0.01);
+
