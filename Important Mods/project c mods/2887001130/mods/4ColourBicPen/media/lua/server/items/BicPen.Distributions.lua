5258a6f2237923e555e8438c9bb2aac42bf17e8f
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-08-23 04:21:11 +0900

    [Chore] Add mod 2887001130

---
+++ Important Mods/project c mods/2887001130/mods/4ColourBicPen/media/lua/server/items/BicPen.Distributions.lua
@@ -0,0 +1,101 @@
+require 'Items/ProceduralDistributions'
+require 'Items/SuburbsDistributions'
+
+table.insert(ProceduralDistributions["list"]["ArtStorePen"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["ArtStorePen"].items, 5);
+
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["BookstoreStationery"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["BookstoreStationery"].items, 5);
+
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["ClassroomMisc"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["ClassroomMisc"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["ClassroomShelves"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["ClassroomShelves"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["CrateOfficeSupplies"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["CrateOfficeSupplies"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["CrateRandomJunk"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["CrateRandomJunk"].items, 0.5);
+
+table.insert(ProceduralDistributions["list"]["DeskGeneric"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["DeskGeneric"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["GigamartSchool"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["GigamartSchool"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["LibraryCounter"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["LibraryCounter"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.8);
+
+table.insert(ProceduralDistributions["list"]["LivingRoomShelfNoTapes"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelfNoTapes"].items, 0.8);
+
+table.insert(ProceduralDistributions["list"]["Locker"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["Locker"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["OfficeCounter"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["OfficeCounter"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["OfficeDesk"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["OfficeDesk"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["OfficeDrawers"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["OfficeDrawers"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["OfficeShelfSupplies"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["OfficeShelfSupplies"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["PoliceDesk"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["PoliceDesk"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["RandomFiller"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["RandomFiller"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["PostOfficeSupplies"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["PostOfficeSupplies"].items, 7);
+
+table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, "BicPen.BicPen");
+table.insert(ProceduralDistributions["list"]["ShelfGeneric"].items, 0.8);
+
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "BicPen.BicPen")
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.8);
+
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "BicPen.BicPen")
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.8);
+
+table.insert(SuburbsDistributions["all"]["counter"].items, "BicPen.BicPen")
+table.insert(SuburbsDistributions["all"]["counter"].items, 7);
+
+table.insert(SuburbsDistributions["all"]["sidetable"].items, "BicPen.BicPen")
+table.insert(SuburbsDistributions["all"]["sidetable"].items, 7);
+
+table.insert(SuburbsDistributions["Bag_WorkerBag"].items, "BicPen.BicPen")
+table.insert(SuburbsDistributions["Bag_WorkerBag"].items, 7);
+
+table.insert(SuburbsDistributions["Bag_Schoolbag"].items, "BicPen.BicPen")
+table.insert(SuburbsDistributions["Bag_Schoolbag"].items, 7);
+
+table.insert(SuburbsDistributions["Briefcase"].items, "BicPen.BicPen")
+table.insert(SuburbsDistributions["Briefcase"].items, 7);
+
+table.insert(SuburbsDistributions["Suitcase"].items, "BicPen.BicPen")
+table.insert(SuburbsDistributions["Suitcase"].items, 7);
