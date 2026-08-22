496088b3d7ba56f553e3660191cddaf961ef0a60
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-08-23 04:37:11 +0900

    [Chore] Add mod 2761200458

---
+++ Important Mods/project c mods/2761200458/mods/Yaki's Hair Salon - BASE/media/lua/shared/Definitions/YakiHairSalonOutfitDefinitions.lua
@@ -0,0 +1,91 @@
+require 'Definitions/HairOutfitDefinitions'
+
+local cat = {};	
+cat.name = "M_Yaki_CroftHair"; 
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_SideBraid";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_Jessica";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_Rochefort";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_CheerLeader";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_LowPonytail";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_DoubleBraids";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_HairBun";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_ClippedHair";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_Pigtails";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_Sherry";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_BraidedPonytail";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_LowPonytailNoFringe";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_LowPonytailBangs";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_HairBunBangs";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_DutchBraids";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "F_Yaki_Dreadlocks";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "M_Yaki_Dreadlocks";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
