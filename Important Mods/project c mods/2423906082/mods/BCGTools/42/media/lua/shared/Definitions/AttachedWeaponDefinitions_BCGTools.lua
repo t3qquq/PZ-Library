81a3ebfb75c0003173bb91ef08507d3339773673
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-08-23 04:30:40 +0900

    [Chore] Add mod 2423906082

---
+++ Important Mods/project c mods/2423906082/mods/BCGTools/42/media/lua/shared/Definitions/AttachedWeaponDefinitions_BCGTools.lua
@@ -0,0 +1,50 @@
+
+
+
+table.insert(AttachedWeaponDefinitions["spearStomach"].weapons, "BCGTools.SpearCheapSurvivalKnife")
+table.insert(AttachedWeaponDefinitions["spearStomach"].weapons, "BCGTools.SpearSurvivalKnife")
+
+table.insert(AttachedWeaponDefinitions["meatCleaverBackLowQuality"].weapons, "BCGTools.HandHatchet")
+table.insert(AttachedWeaponDefinitions["meatCleaverBackLowQuality"].weapons, "BCGTools.SplittingAxe")
+
+table.insert(AttachedWeaponDefinitions["meatCleaverBack"].weapons, "BCGTools.KukriMachete")
+table.insert(AttachedWeaponDefinitions["meatCleaverBack"].weapons, "BCGTools.HandHatchet")
+table.insert(AttachedWeaponDefinitions["meatCleaverBack"].weapons, "BCGTools.SplittingAxe")
+table.insert(AttachedWeaponDefinitions["meatCleaverBack"].weapons, "BCGTools.RancherMachete")
+
+table.insert(AttachedWeaponDefinitions["axeBack"].weapons, "BCGTools.SplittingAxe")
+
+table.insert(AttachedWeaponDefinitions["knifeBack"].weapons, "BCGTools.SurvivalKnife")
+table.insert(AttachedWeaponDefinitions["knifeBack"].weapons, "BCGTools.CheapSurvivalKnife")
+
+table.insert(AttachedWeaponDefinitions["knifeLeftLeg"].weapons, "BCGTools.SurvivalKnife")
+table.insert(AttachedWeaponDefinitions["knifeLeftLeg"].weapons, "BCGTools.CheapSurvivalKnife")
+
+table.insert(AttachedWeaponDefinitions["knifeRightLeg"].weapons, "BCGTools.SurvivalKnife")
+table.insert(AttachedWeaponDefinitions["knifeRightLeg"].weapons, "BCGTools.CheapSurvivalKnife")
+
+table.insert(AttachedWeaponDefinitions["knifeShoulder"].weapons, "BCGTools.SurvivalKnife")
+table.insert(AttachedWeaponDefinitions["knifeShoulder"].weapons, "BCGTools.CheapSurvivalKnife")
+
+table.insert(AttachedWeaponDefinitions["MacheteShoulder"].weapons, "BCGTools.KukriMachete")
+
+table.insert(AttachedWeaponDefinitions["knifeStomach"].weapons, "BCGTools.SurvivalKnife")
+table.insert(AttachedWeaponDefinitions["knifeStomach"].weapons, "BCGTools.CheapSurvivalKnife")
+
+table.insert(AttachedWeaponDefinitions["lumberjack"].weapons, "BCGTools.SplittingAxe")
+table.insert(AttachedWeaponDefinitions["lumberjack"].weapons, "BCGTools.HandHatchet")
+
+table.insert(AttachedWeaponDefinitions["hammerBelt_Early"].weapons, "BCGTools.SplittingAxe")
+table.insert(AttachedWeaponDefinitions["hammerBelt_Early"].weapons, "BCGTools.HandHatchet")
+
+table.insert(AttachedWeaponDefinitions["hammerBelt_Mid"].weapons, "BCGTools.SplittingAxe")
+table.insert(AttachedWeaponDefinitions["hammerBelt_Mid"].weapons, "BCGTools.HandHatchet")
+
+table.insert(AttachedWeaponDefinitions["hammerBelt_Late"].weapons, "BCGTools.SplittingAxe")
+table.insert(AttachedWeaponDefinitions["hammerBelt_Late"].weapons, "BCGTools.HandHatchet")
+
+table.insert(AttachedWeaponDefinitions["hammerBelt_Bandit"].weapons, "BCGTools.SplittingAxe")
+table.insert(AttachedWeaponDefinitions["hammerBelt_Bandit"].weapons, "BCGTools.HandHatchet")
+
+table.insert(AttachedWeaponDefinitions["macheteInBack"].weapons, "BCGTools.KukriMachete")
+table.insert(AttachedWeaponDefinitions["macheteInBack"].weapons, "BCGTools.RancherMachete")
