db478ff394504bf2f26a9e40590de48ada376186
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-08-23 04:42:57 +0900

    [Chore] Add mod 2976105553

---
+++ Important Mods/project c mods/2976105553/mods/wypickupbag/42/media/lua/client/wypickupbag.lua
@@ -0,0 +1,96 @@
+function ISInventoryTransferAction:isValid()
+
+	if not self.item then
+		return false;
+    end
+	self.dontAdd = false;
+	if not self.destContainer or not self.srcContainer then return false; end
+	if self.allowMissingItems and not self.srcContainer:contains(self.item) then -- if the item is destroyed before, for example when crafting something, we want to transfer the items left back to their original position, but some might be destroyed by the recipe (like molotov, the gas can will be returned, but the ripped sheet is destroyed)
+--		self:stop();
+		self.dontAdd = true;
+		return true;
+	end
+	if (not self.destContainer:isExistYet()) or (not self.srcContainer:isExistYet()) then
+		return false
+	end
+
+	local parent = self.srcContainer:getParent()
+	-- Duplication exploit: drag items from a corpse to another container while pickup up the corpse.
+	-- ItemContainer:isExistYet() would detect this if SystemDisabler.doWorldSyncEnable was true.
+	if instanceof(parent, "IsoDeadBody") and parent:getStaticMovingObjectIndex() == -1 then
+		return false
+	end
+
+	-- Don't fail if the item was transferred by a previous action.
+	if self:isAlreadyTransferred(self.item) then
+		return true
+	end
+
+	-- Limit items per container in MP
+	if isClient() then
+		if not isItemTransactionConsistent(self.item, self.srcContainer, self.destContainer) then
+			return false
+		end
+		local limit = getServerOptions():getInteger("ItemNumbersLimitPerContainer");
+		if limit > 0 and (not instanceof(self.destContainer:getParent(), "IsoGameCharacter")) then
+			--allow dropping full bags on an empty square or put full container in an empty container
+			if not self.destContainer:getItems():isEmpty() then
+				local destRoot = self:findRootInventory(self.destContainer);
+				local srcRoot = self:findRootInventory(self.srcContainer);
+				--total count remains the same if the same root container
+				if srcRoot ~= destRoot then
+					local tranferItemsNum = 1;
+					if self.item:getCategory() == "Container" then
+						tranferItemsNum = self:countItemsRecursive({self.item:getInventory()}, 1);
+					end;
+					--count items from the root container
+					local destContainerItemsNum = self:countItemsRecursive({destRoot}, 0);
+					--if destination is an item then add 1
+					if destRoot:getContainingItem() then destContainerItemsNum = destContainerItemsNum + 1; end;
+					--total items must not exceed the server limit
+					if destContainerItemsNum + tranferItemsNum > limit then
+						return false;
+					end;
+				end;
+			end;
+		end;
+	end;
+
+    if ISTradingUI.instance and ISTradingUI.instance:isVisible() then
+        return false;
+	end
+	if not self.srcContainer:contains(self.item) then
+		return false;
+    end
+    if self.srcContainer == self.destContainer then return false; end
+
+    if self.destContainer:getType()=="floor" then
+        if instanceof(self.item, "Moveable") and self.item:getSpriteGrid()==nil then
+            if not self.item:CanBeDroppedOnFloor() then
+                return false;
+            end
+        end
+        if self:getNotFullFloorSquare(self.item) == nil then
+            return false;
+        end
+    elseif not self.destContainer:hasRoomFor(self.character, self.item) then
+		if self.item:getCategory() == "Container" then
+			return true
+		end
+        return false;
+    end
+
+    if not self.srcContainer:isRemoveItemAllowed(self.item) then
+        return false;
+    end
+    if not self.destContainer:isItemAllowed(self.item) then
+        return false;
+    end
+    if self.item:getContainer() == self.srcContainer and not self.destContainer:isInside(self.item) then
+        return true;
+    end
+    if isClient() and self.srcContainer:getSourceGrid() and SafeHouse.isSafeHouse(self.srcContainer:getSourceGrid(), self.character:getUsername(), true) then
+        return false;
+	end
+    return false;
+end
