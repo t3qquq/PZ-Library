4d62c6290045c9a002faae9d5d12cec70914a36d
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-08-23 04:36:48 +0900

    [Chore] Add mod 2945680537

---
+++ Important Mods/project c mods/2945680537/mods/synewhairmod/media/lua/shared/Definitions/syMOD_HairOutfitDefinitions.lua
@@ -0,0 +1,44 @@
+require 'Definitions/HairOutfitDefinitions'
+
+
+local cat = {};
+cat.name = "IdolHair1";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "IdolHair2";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "IdolHair3";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "IdolHair4";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "IdolHair5";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "IdolHair6";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "IdolHair7";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "IdolHair8";
+cat.onlyFor = "ZombiesDontLike";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
