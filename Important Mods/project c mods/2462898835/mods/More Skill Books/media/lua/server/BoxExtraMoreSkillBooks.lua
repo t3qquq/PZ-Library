a7901b27759353098f341116ce8270e71ed664dc
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-08-23 04:35:13 +0900

    [Chore] Add mod 2462898835

---
+++ Important Mods/project c mods/2462898835/mods/More Skill Books/media/lua/server/BoxExtraMoreSkillBooks.lua
@@ -0,0 +1,190 @@
+--Return additional Books upon opening --
+function Box_OpenTrapping(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookTrapping2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookTrapping3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookTrapping4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookTrapping5");      
+	      					
+end
+function Box_OpenFishing(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookFishing2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFishing3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFishing4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFishing5");      
+	      					
+end
+function Box_OpenCarpentry(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookCarpentry2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookCarpentry3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookCarpentry4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookCarpentry5");      
+	      					
+end
+function Box_OpenMechanic(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookMechanic2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookMechanic3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookMechanic4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookMechanic5");      
+	      					
+end
+function Box_OpenFirstAid(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookFirstAid2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFirstAid3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFirstAid4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFirstAid5");      
+	      					
+end
+function Box_OpenBlacksmith(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookBlacksmith2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookBlacksmith3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookBlacksmith4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookBlacksmith5");      
+	      					
+end
+function Box_OpenMetalWelding(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookMetalWelding2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookMetalWelding3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookMetalWelding4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookMetalWelding5");      
+	      					
+end
+function Box_OpenElectrician(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookElectrician2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookElectrician3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookElectrician4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookElectrician5");      
+	      					
+end
+function Box_OpenCooking(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookCooking2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookCooking3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookCooking4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookCooking5");      
+	      					
+end
+function Box_OpenFarming(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookFarming2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFarming3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFarming4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFarming5");      
+	      					
+end
+function Box_OpenForaging(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookForaging2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookForaging3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookForaging4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookForaging5");      
+	      					
+end
+function Box_OpenTailoring(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookTailoring2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookTailoring3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookTailoring4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookTailoring5");      
+	      					
+end
+function Box_OpenFitness(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookFitness2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFitness3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFitness4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookFitness5");      
+	      					
+end
+function Box_OpenStrength(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookStrength2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookStrength3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookStrength4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookStrength5");      
+	      					
+end
+function Box_OpenAxe(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookAxe2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookAxe3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookAxe4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookAxe5");      
+	      					
+end
+function Box_OpenLongBlunt(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookLongBlunt2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookLongBlunt3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookLongBlunt4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookLongBlunt5");      
+	      					
+end
+function Box_OpenShortBlunt(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookShortBlunt2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookShortBlunt3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookShortBlunt4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookShortBlunt5");      
+	      					
+end
+function Box_OpenLongBlade(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookLongBlade2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookLongBlade3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookLongBlade4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookLongBlade5");      
+	      					
+end
+function Box_OpenShortBlade(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookShortBlade2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookShortBlade3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookShortBlade4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookShortBlade5");      
+	      					
+end
+function Box_OpenSpear(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookSpear2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookSpear3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookSpear4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookSpear5");      
+	      					
+end
+function Box_OpenMaintenance(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookMaintenance2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookMaintenance3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookMaintenance4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookMaintenance5");      
+	      					
+end
+function Box_OpenSprinting(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookSprinting2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookSprinting3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookSprinting4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookSprinting5");      
+	      					
+end
+function Box_OpenLightfooted(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookLightfooted2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookLightfooted3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookLightfooted4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookLightfooted5");      
+	      					
+end
+function Box_OpenNimble(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookNimble2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookNimble3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookNimble4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookNimble5");      
+	      					
+end
+function Box_OpenSneaking(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookSneaking2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookSneaking3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookSneaking4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookSneaking5");      
+	      					
+end
+function Box_OpenAiming(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookAiming2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookAiming3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookAiming4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookAiming5");      
+	      					
+end
+function Box_OpenReloading(items, result, player)
+    player:getInventory():AddItem("MoreSkillBooks.BookReloading2");      
+	player:getInventory():AddItem("MoreSkillBooks.BookReloading3");      
+	player:getInventory():AddItem("MoreSkillBooks.BookReloading4");      
+	player:getInventory():AddItem("MoreSkillBooks.BookReloading5");      
+	      					
+end
