ccf1b486c83d4ea805eb536f3300f8bcd5919547
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-08-23 04:36:39 +0900

    [Chore] Add mod 3026307069

---
+++ Important Mods/project c mods/3026307069/mods/ModTemplate/media/lua/shared/Definitions/FrontierHairRemoval.lua
@@ -0,0 +1,330 @@
+require 'Definitions/HairOutfitDefinitions'
+
+local cat = {};
+cat.name = "Arkenaux";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Ada";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Mango_WildFire";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Riven_WildFire";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Parrallax";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Riven";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "LINCL";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Mango";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "FennexWildFireClassy";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Fennex";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "FennexAlt";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "FennexSurvivor";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "FennexWildFire";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "FennexParadigm";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "FennexRogue";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Maemento";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "UmbralPheonix";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Nexus";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "SeaBreeze";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Autumn";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Ark";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Fate";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Clara";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Carter";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Kitara";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "DateNight";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Carbonite";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "SecondDream";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Valiant";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Illenium";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Accursed";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Evergent";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "CountDown";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Abbadon";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Azazel";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "UnConstrained";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Rememberance";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "SummerTime";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "MangoReversed";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Naxium";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Warranted";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Evelyn";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "KeyToMyHeart";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "frontierGP2";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Cirrium";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Loyalty";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "AfterDark";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "frontierGP3";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "SomberMoments";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "AlteredReflections";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Rufflet";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Avantime";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+local cat = {};
+cat.name = "Fawne";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+local cat = {};
+cat.name = "Fawne";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
+
+
+
+local cat = {};
+cat.name = "Scarlet";
+cat.onlyFor = "FakeZ";
+table.insert(HairOutfitDefinitions.haircutDefinition, cat);
