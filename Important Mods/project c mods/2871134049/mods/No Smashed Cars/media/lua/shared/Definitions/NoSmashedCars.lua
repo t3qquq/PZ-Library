6936c4be682b929f8f6bbf1e428c153d9f995968
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-08-23 04:42:50 +0900

    [Chore] Add mod 2871134049

---
+++ Important Mods/project c mods/2871134049/mods/No Smashed Cars/media/lua/shared/Definitions/NoSmashedCars.lua
@@ -0,0 +1,120 @@
+SmashedCarDefinitions.cars["Base.CarNormal"].front = nil;
+SmashedCarDefinitions.cars["Base.CarNormal"].rear = nil;
+SmashedCarDefinitions.cars["Base.CarNormal"].left = nil;
+SmashedCarDefinitions.cars["Base.CarNormal"].right = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpTruck"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruck"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruck"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruck"].left = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpTruckMccoy"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruckMccoy"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruckMccoy"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruckMccoy"].left = nil;
+
+SmashedCarDefinitions.cars["Base.ModernCar"].rear = nil;
+SmashedCarDefinitions.cars["Base.ModernCar"].front = nil;
+SmashedCarDefinitions.cars["Base.ModernCar"].right = nil;
+SmashedCarDefinitions.cars["Base.ModernCar"].left = nil;
+
+SmashedCarDefinitions.cars["Base.ModernCar02"].rear = nil;
+SmashedCarDefinitions.cars["Base.ModernCar02"].front = nil;
+SmashedCarDefinitions.cars["Base.ModernCar02"].right = nil;
+SmashedCarDefinitions.cars["Base.ModernCar02"].left = nil;
+
+SmashedCarDefinitions.cars["Base.CarLights"].rear = nil;
+SmashedCarDefinitions.cars["Base.CarLights"].front = nil;
+SmashedCarDefinitions.cars["Base.CarLights"].right = nil;
+SmashedCarDefinitions.cars["Base.CarLights"].left = nil;
+
+SmashedCarDefinitions.cars["Base.CarLightsPolice"].rear = nil;
+SmashedCarDefinitions.cars["Base.CarLightsPolice"].front = nil;
+SmashedCarDefinitions.cars["Base.CarLightsPolice"].right = nil;
+SmashedCarDefinitions.cars["Base.CarLightsPolice"].left = nil;
+SmashedCarDefinitions.cars["Base.CarLightsPolice"].skin = 1;
+
+SmashedCarDefinitions.cars["Base.CarStationWagon"].rear = nil;
+SmashedCarDefinitions.cars["Base.CarStationWagon"].front = nil;
+SmashedCarDefinitions.cars["Base.CarStationWagon"].right = nil;
+SmashedCarDefinitions.cars["Base.CarStationWagon"].left = nil;
+
+SmashedCarDefinitions.cars["Base.CarStationWagon2"].rear = nil;
+SmashedCarDefinitions.cars["Base.CarStationWagon2"].front = nil;
+SmashedCarDefinitions.cars["Base.CarStationWagon2"].right = nil;
+SmashedCarDefinitions.cars["Base.CarStationWagon2"].left = nil;
+
+SmashedCarDefinitions.cars["Base.CarLuxury"].rear = nil;
+SmashedCarDefinitions.cars["Base.CarLuxury"].front = nil;
+SmashedCarDefinitions.cars["Base.CarLuxury"].right = nil;
+SmashedCarDefinitions.cars["Base.CarLuxury"].left = nil;
+
+SmashedCarDefinitions.cars["Base.OffRoad"].rear = nil;
+SmashedCarDefinitions.cars["Base.OffRoad"].front = nil;
+SmashedCarDefinitions.cars["Base.OffRoad"].right = nil;
+SmashedCarDefinitions.cars["Base.OffRoad"].left = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpTruckLights"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruckLights"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruckLights"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruckLights"].left = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpTruckLightsFire"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruckLightsFire"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruckLightsFire"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpTruckLightsFire"].left = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpVan"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpVan"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpVan"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpVan"].left = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpVanMccoy"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanMccoy"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanMccoy"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanMccoy"].left = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpVanLights"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLights"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLights"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLights"].left = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpVanLights"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLights"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLights"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLights"].left = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpVanLightsFire"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLightsFire"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLightsFire"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLightsFire"].left = nil;
+
+SmashedCarDefinitions.cars["Base.PickUpVanLightsPolice"].rear = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLightsPolice"].front = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLightsPolice"].right = nil;
+SmashedCarDefinitions.cars["Base.PickUpVanLightsPolice"].left = nil;
+
+SmashedCarDefinitions.cars["Base.SmallCar"].rear = nil;
+SmashedCarDefinitions.cars["Base.SmallCar"].front = nil;
+SmashedCarDefinitions.cars["Base.SmallCar"].right = nil;
+SmashedCarDefinitions.cars["Base.SmallCar"].left = nil;
+
+SmashedCarDefinitions.cars["Base.SmallCar02"].rear = nil;
+SmashedCarDefinitions.cars["Base.SmallCar02"].front = nil;
+SmashedCarDefinitions.cars["Base.SmallCar02"].right = nil;
+SmashedCarDefinitions.cars["Base.SmallCar02"].left = nil;
+
+SmashedCarDefinitions.cars["Base.StepVan"].rear = nil;
+SmashedCarDefinitions.cars["Base.StepVan"].front = nil;
+SmashedCarDefinitions.cars["Base.StepVan"].right = nil;
+SmashedCarDefinitions.cars["Base.StepVan"].left = nil;
+
+SmashedCarDefinitions.cars["Base.StepVanMail"].rear = nil;
+SmashedCarDefinitions.cars["Base.StepVanMail"].front = nil;
+SmashedCarDefinitions.cars["Base.StepVanMail"].right = nil;
+SmashedCarDefinitions.cars["Base.StepVanMail"].left = nil;
+
+SmashedCarDefinitions.cars["Base.SUV"].rear = nil;
+SmashedCarDefinitions.cars["Base.SUV"].front = nil;
+SmashedCarDefinitions.cars["Base.SUV"].right = nil;
+SmashedCarDefinitions.cars["Base.SUV"].left = nil;
